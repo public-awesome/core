55d6430d87b6d010e017c500e11176c92c60e3dc
Shane Vitarana <shanev@gmail.com>
2023-06-07 23:48:31 -0400

    Metadata collection

--- contracts/loyalty/collection/src/msg.rs
+++ contracts/loyalty/collection/src/msg.rs
@@ -1,11 +1,25 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::Coin;
+
+use crate::state::Metadata;
 
 #[cw_serde]
-pub struct InstantiateMsg {}
+pub struct InstantiateMsg {
+    pub owner: String,
+}
 
 #[cw_serde]
-pub enum ExecuteMsg {}
+pub enum ExecuteMsg {
+    UpdateMetadata {
+        address: String,
+        staked_amount: Coin,
+        data: Option<String>,
+    },
+}
 
 #[cw_serde]
 #[derive(QueryResponses)]
-pub enum QueryMsg {}
+pub enum QueryMsg {
+    #[returns(Metadata)]
+    Metadata { address: String },
+}
--- contracts/loyalty/collection/src/state.rs
+++ contracts/loyalty/collection/src/state.rs
@@ -1,1 +1,9 @@
+use cosmwasm_schema::cw_serde;
+use cosmwasm_std::Coin;
 
+#[cw_serde]
+pub struct Metadata {
+    pub staked_amount: Coin,
+    pub data: Option<String>,
+    pub updated_at: u64,
+}
