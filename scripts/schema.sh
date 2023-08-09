c17d2bd2e35fbfeb9e0f151240dd95b1ec9dac5d
Shane Vitarana <shanev@gmail.com>
2023-08-09 11:25:54 -0400

    Generate schema

--- scripts/schema.sh
+++ scripts/schema.sh
@@ -1,13 +1,14 @@
-# for d in contracts/*; do
-#   if [ -d "$d" ]; then
-#     cd $d
-#     cargo schema
-#     rm -rf schema/raw
-#     cd ../..
-#   fi
-# done
-
 cd contracts/fair-burn
 cargo schema
 rm -rf schema/raw
 cd ../..
+
+for d in contracts/vip/*; do
+  if [ -d "$d" ]; then
+    cd $d
+    cargo schema
+    rm -rf schema/raw
+    cd ../../..
+  fi
+done
+
