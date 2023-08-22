05122492d0026fc0ee215fc35f0dba35f4fb5ba5
Shane Vitarana <shanev@gmail.com>
2023-08-22 00:15:40 -0400

    Add coin and msg helpers

--- packages/sg-std/src/lib.rs
+++ packages/sg-std/src/lib.rs
@@ -1,4 +1,3 @@
-pub mod math;
 mod msg;
 mod query;
 mod route;
@@ -7,6 +6,7 @@ pub const NATIVE_DENOM: &str = "ustars";
 // 3/11/2022 16:00:00 ET
 pub const GENESIS_MINT_START_TIME: u64 = 1647032400000000000;
 
+use cosmwasm_std::{coin, coins, Addr, BankMsg, Coin};
 pub use msg::{
     create_claim_for_msg, create_fund_community_pool_msg, create_fund_fairburn_pool_msg,
     ClaimAction, StargazeMsg, StargazeMsgWrapper,
@@ -23,3 +23,18 @@ pub use route::StargazeRoute;
 // "stargaze" support on the chain they run on.
 #[no_mangle]
 extern "C" fn requires_stargaze() {}
+
+pub fn stars(amount: impl Into<u128>) -> Vec<Coin> {
+    coins(amount.into(), NATIVE_DENOM)
+}
+
+pub fn star(amount: impl Into<u128>) -> Coin {
+    coin(amount.into(), NATIVE_DENOM)
+}
+
+pub fn send_msg(to_address: &Addr, amount: impl Into<u128>) -> BankMsg {
+    BankMsg::Send {
+        to_address: to_address.to_string(),
+        amount: stars(amount),
+    }
+}
