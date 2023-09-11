313b6c5348a079ce77f1de7e793f833b1a571370
Serkan Reis <serkanreis@gmail.com>
2023-09-11 16:13:56 +0300

    Test collection/execute()

--- contracts/vip/collection/src/contract.rs
+++ contracts/vip/collection/src/contract.rs
@@ -2,7 +2,7 @@
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
 use cw2::set_contract_version;
-use cw721::{Cw721Query};
+use cw721::Cw721Query;
 use cw721_base::InstantiateMsg;
 
 use crate::error::ContractError;
@@ -64,15 +64,119 @@ pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
 
 #[cfg(test)]
 mod tests {
-    use cosmwasm_std::Empty;
-    use cw_multi_test::{Contract, ContractWrapper};
+    use crate::state::Metadata;
+    use crate::ContractError;
+    use cosmwasm_std::{Addr, Empty, Event, Timestamp, Uint128};
+    use cw721_base::InstantiateMsg;
+    use cw_multi_test::{App, AppResponse, Contract, ContractWrapper, Executor};
 
     fn collection_contract() -> Box<dyn Contract<Empty>> {
         let contract = ContractWrapper::new(
-            crate::contract::instantiate,
             crate::contract::execute,
+            crate::contract::instantiate,
             crate::contract::query,
         );
         Box::new(contract)
+    }
+
+    fn find_event<'a>(response: &'a AppResponse, event_type: &'a str) -> Option<&'a Event> {
+        response.events.iter().find(|event| event.ty == event_type)
+    }
+
+    fn find_attribute(event: &Event, key: &str) -> Option<String> {
+        event
+            .attributes
+            .iter()
+            .find(|attr| attr.key == key)
+            .map(|attr| attr.value.clone())
+    }
+
+    #[test]
+    fn try_instantiate() {
+        let mut app = App::default();
+        let vip_collection_code_id = app.store_code(collection_contract());
+
+        let creator = Addr::unchecked("creator");
+
+        let init_msg = InstantiateMsg {
+            name: "Stargaze VIP Collection".to_string(),
+            symbol: "SGVIP".to_string(),
+            minter: Addr::unchecked("minter").to_string(),
+        };
+
+        let response = app.instantiate_contract(
+            vip_collection_code_id,
+            creator,
+            &init_msg,
+            &[],
+            "vip collection",
+            None,
+        );
+        assert!(response.is_ok());
+    }
+
+    #[test]
+    fn try_execute() {
+        let mut app = App::default();
+        let vip_collection_code_id = app.store_code(collection_contract());
+
+        let creator = Addr::unchecked("creator");
+
+        let init_msg = InstantiateMsg {
+            name: "Stargaze VIP Collection".to_string(),
+            symbol: "SGVIP".to_string(),
+            minter: Addr::unchecked("minter").to_string(),
+        };
+
+        let collection_address = app
+            .instantiate_contract(
+                vip_collection_code_id,
+                creator,
+                &init_msg,
+                &[],
+                "vip collection",
+                None,
+            )
+            .unwrap();
+
+        let mint_msg: cw721_base::ExecuteMsg<Metadata, Empty> = cw721_base::ExecuteMsg::Mint {
+            token_id: "1".to_string(),
+            owner: Addr::unchecked("owner").to_string(),
+            token_uri: None,
+            extension: Metadata {
+                staked_amount: Uint128::new(10000000000),
+                data: None,
+                updated_at: Timestamp::from_seconds(100),
+            },
+        };
+
+        let response = app.execute_contract(
+            Addr::unchecked("minter"),
+            collection_address.clone(),
+            &mint_msg,
+            &[],
+        );
+        assert!(response.is_ok());
+        let app_response = response.unwrap();
+        let event = find_event(&app_response, "wasm").unwrap();
+        let action = find_attribute(&event, "action").unwrap();
+        assert_eq!(action, "mint");
+
+        let transfer_msg: cw721_base::ExecuteMsg<Metadata, Empty> =
+            cw721_base::ExecuteMsg::TransferNft {
+                recipient: Addr::unchecked("recipient").to_string(),
+                token_id: "1".to_string(),
+            };
+
+        let response = app
+            .execute_contract(
+                Addr::unchecked("owner"),
+                collection_address,
+                &transfer_msg,
+                &[],
+            )
+            .map_err(|e| e.downcast::<ContractError>().unwrap())
+            .unwrap_err();
+        assert!(matches!(response, ContractError::Unauthorized {}));
     }
 }
