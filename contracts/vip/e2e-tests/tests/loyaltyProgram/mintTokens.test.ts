df2533661a10bf0c6e61cfc04e327932631d31f5
Serkan Reis <serkanreis@gmail.com>
2023-10-23 14:58:34 +0300

    Add test for tier updates

--- contracts/vip/e2e-tests/tests/loyaltyProgram/mintTokens.test.ts
+++ contracts/vip/e2e-tests/tests/loyaltyProgram/mintTokens.test.ts
@@ -1,37 +1,42 @@
 import { CosmWasmClient } from '@cosmjs/cosmwasm-stargate'
-import { toUtf8 } from '@cosmjs/encoding'
 import { denom } from '../../configs/chain_config.json'
 import Context, { CONTRACT_MAP, TestUser } from '../setup/context'
 import { getQueryClient } from '../utils/client'
 import { ArrayOfUint128, ExecuteMsg as MinterExecuteMsg } from '../types/minter.types'
-import { ExecuteMsg as CollectionExecuteMsg } from '../types/collection.types'
 import { MinterQueryClient } from '../types/minter.client'
 import { CollectionQueryClient } from '../types/collection.client'
-import { MsgExecuteContract } from 'cosmjs-types/cosmwasm/wasm/v1/tx'
 import _ from 'lodash'
 import { coin } from '@cosmjs/proto-signing'
+import { StakedTokensAPIResponse } from '../utils/stake'
 
 describe('Mint Loyalty Program Tokens', () => {
+  // userOne is the minter contract owner
   const userOne = 'user1'
   const userTwo = 'user2'
 
   let context: Context
   let queryClient: CosmWasmClient
   let minterQueryClient: MinterQueryClient
+  let collectionQueryClient: CollectionQueryClient
   let minterAddress: string
+  let collectionAddress: string
   let tiers: ArrayOfUint128
   let testUserOne: TestUser
+  let testUserTwo: TestUser
 
   beforeAll(async () => {
     context = new Context()
     await context.initialize(true)
     minterAddress = context.getContractAddress(CONTRACT_MAP.VIP_MINTER)
+    collectionAddress = context.getContractAddress(CONTRACT_MAP.VIP_COLLECTION)
 
     queryClient = await getQueryClient()
 
     minterQueryClient = new MinterQueryClient(queryClient, minterAddress)
+    collectionQueryClient = new CollectionQueryClient(queryClient, collectionAddress)
     tiers = await minterQueryClient.tiers()
     testUserOne = context.getTestUser(userOne)
+    testUserTwo = context.getTestUser(userTwo)
   })
 
   test('Mint Initial Token', async () => {
@@ -57,6 +62,14 @@ describe('Mint Loyalty Program Tokens', () => {
 
     let userOneTier = await minterQueryClient.tier({ address: testUserOne.address })
     expect(userOneTier).toBe(0)
+
+    const tokenMetadata = await collectionQueryClient.nftInfo({ tokenId: "1" })
+    const stakedTokensResponse: Response = await fetch(
+      `https://rest.elgafar-1.stargaze-apis.com/cosmos/staking/v1beta1/delegations/${testUserOne.address}`,
+    )
+    const stakedTokens: StakedTokensAPIResponse = await stakedTokensResponse.json()
+    const stakedAmount = stakedTokens.delegation_responses[0]?.balance?.amount
+    expect(tokenMetadata.extension.staked_amount).toBe(stakedAmount)
   })
 
   test('Update Token', async () => {
@@ -84,6 +97,14 @@ describe('Mint Loyalty Program Tokens', () => {
     
     let userOneTier = await minterQueryClient.tier({ address: testUserOne.address })
     expect(userOneTier).toBe(1)
+
+    const tokenMetadata = await collectionQueryClient.nftInfo({ tokenId: "1" })
+    const stakedTokensResponse: Response = await fetch(
+      `https://rest.elgafar-1.stargaze-apis.com/cosmos/staking/v1beta1/delegations/${testUserOne.address}`,
+    )
+    const stakedTokens: StakedTokensAPIResponse = await stakedTokensResponse.json()
+    const stakedAmount = stakedTokens.delegation_responses[0]?.balance?.amount
+    expect(tokenMetadata.extension.staked_amount).toBe(stakedAmount)
   })
 
   test('Fail to Mint Duplicate Tokens', async () => {
@@ -100,4 +121,54 @@ describe('Mint Loyalty Program Tokens', () => {
     )).rejects.toThrowError(/AlreadyMinted/)
   }
   )
+
+  test('Update tiers', async () => {
+    const stakedTokensResponse: Response = await fetch(
+      `https://rest.elgafar-1.stargaze-apis.com/cosmos/staking/v1beta1/delegations/${testUserOne.address}`,
+    )
+    const stakedTokens: StakedTokensAPIResponse = await stakedTokensResponse.json()
+    const stakedAmount = stakedTokens.delegation_responses[0]?.balance?.amount
+    
+    const updateTiersMsg: MinterExecuteMsg = {
+      update_tiers: {
+        tiers: [(Number(stakedAmount) - 500).toString(), (Number(stakedAmount) - 100).toString(), (Number(stakedAmount) + 100).toString()],
+      },
+    }
+
+    await expect(testUserTwo.client.execute(
+      testUserTwo.address,
+      minterAddress,
+      updateTiersMsg,
+      'auto',
+      'update tiers',
+    )).rejects.toThrowError(/Unauthorized/)
+
+    await testUserOne.client.execute(
+      testUserOne.address,
+      minterAddress,
+      updateTiersMsg,
+      'auto',
+      'update tiers',
+    )
+
+    const tiers = await minterQueryClient.tiers()
+    expect(tiers).toEqual([(Number(stakedAmount) - 500).toString(), (Number(stakedAmount) - 100).toString(), (Number(stakedAmount) + 100).toString()])
+
+    const updateMsg: MinterExecuteMsg = {
+      update: {
+        token_id: 1,
+      }
+    }
+    // Any address can update tokens
+    await testUserTwo.client.execute(
+      testUserTwo.address,
+      minterAddress,
+      updateMsg,
+      'auto',
+      'update token',
+    )
+    
+    let userOneTier = await minterQueryClient.tier({ address: testUserOne.address })
+    expect(userOneTier).toBe(2)
+  })
 })
